7bbd73e8c3141634ce9310cf1dbe6c26b7b50b5c
Mert Gül <ibrahimmertgul@gmail.com>
2020-10-07 11:41:34 +0300

    service added

--- main.js
+++ main.js
@@ -1,7 +1,48 @@
 const { rejects } = require("assert");
 const { app, BrowserWindow, ipcMain } = require("electron");
 const fs = require("fs");
-const path = require("path");
+var express = require("express"),
+  path = require("path"),
+  service = express();
+
+
+
+service.set("port", process.env.PORT || 3000);
+service.use(express.static("public"));
+service.listen(service.get("port"), function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Running on port: " + service.get("port"));
+  }
+});
+service.use(
+  express.urlencoded({
+    extend: false,
+  })
+);
+service.use(express.json());
+
+service.post('/upload/:filename', function (req, res) {
+  var filename = path.basename(req.params.filename);
+  filename = path.resolve('media', filename);
+  var dst = fs.createWriteStream(filename);
+  req.pipe(dst);
+  dst.on('drain', function() {
+    console.log('drain', new Date());
+    req.resume();
+  });
+  req.on('end', function () {
+    mainWindow.webContents.send("file", filename);
+    res.send(200);
+  });
+});
+
+//***************************************************************//
+//***************************************************************//
+//***********************   ELECTRON  ***************************//
+//***************************************************************//
+//***************************************************************//
 let mainWindow;
 
 function createWindow() {
@@ -16,23 +57,20 @@ function createWindow() {
     },
   });
 
-  mainWindow.loadFile("index.html");
-  // mainWindow.webContents.openDevTools();
+  mainWindow.loadFile("window.html");
+  mainWindow.webContents.openDevTools();
 }
 
-app
-  .whenReady()
-  .then(() => {
-    createWindow();
-  })
-  // .then(() => {
-  //   mainWindow.webContents.on("did-finish-load", () => {
-  //     fs.readdir(path.join(__dirname, "./media"), function (err, dir) {
-  //       if (err) rejects(err);
-  //       else {
-  //         mainWindow.webContents.send("files", dir);
-  //       }
-  //     });
-  //   });
-  // });
-
+app.whenReady().then(() => {
+  createWindow();
+});
+// .then(() => {
+//   mainWindow.webContents.on("did-finish-load", () => {
+//     fs.readdir(path.join(__dirname, "./media"), function (err, dir) {
+//       if (err) rejects(err);
+//       else {
+//         mainWindow.webContents.send("files", dir);
+//       }
+//     });
+//   });
+// });
